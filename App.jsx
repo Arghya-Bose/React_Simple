c89f3d68c48aae23549fb63bbe8e8be76deb4c82
ARGHYA BOSE <136734096+Arghya-Bose@users.noreply.github.com>
2025-07-13 14:48:09 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,33 +1,16 @@
-//This code for simple React Form operation
-
 import { useState } from "react";
 import "./App.css";
 import Login, { Profile, Setting } from "./UserComponent";
+import CheckBox from "./CheckBox";
 
 
 function App() {
-  const[name, setName] = useState("");
-  const[password, setPassword] = useState("");
-  const[email, setEmail] = useState("");
+
 
   return (
-    <div  >
-      <h1>Welcome to React</h1>
-      <form action="" method="get">
-        <input type="text" value = {name} onChange={(event)=> setName(event.target.value)} placeholder="Enter name" />
-        <br /><br />
-
-        <input type ="password" value={password} onChange={(event)=>setPassword(event.target.value)} placeholder="enter password"/>
-        <br /><br />
-
-        <input type="email" value={email} onChange={(event)=>setEmail(event.target.value)} placeholder="enter email"/>
-        <br /><br />
-        <button >Submit</button>
-        <button onClick={()=>{setName(""); setEmail(""); setPassword("");}}>Clear</button>
-      </form>
-      <h1>{name}</h1>
-      <h1>{password}</h1>
-      <h1>{email}</h1>
+    <div >
+      <h1>React Check Box</h1>
+      <CheckBox />
     </div>
   );
 }
